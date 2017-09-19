77648705425b82f20eaeee61ec77ccf3fe19e478
Mike Prentice <michaelprentice@lovepopcards.com>
2017-09-19 12:55:40 -0400

    Update versions in requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='tap-mailchimp',
       version='0.1.a.dev',
-      install_requires=['python-dateutil>=2.6.0',
-                        'singer-python>=2.1.4',
-                        'mailchimp3>=2.0.15',
-                        'requests>=2.18.3'],
+      install_requires=['python-dateutil',
+                        'singer-python',
+                        'mailchimp3',
+                        'requests'],
       entry_points={
           'console_scripts': ['tap-mailchimp = tap_mailchimp:main']
       })
